4238cf737346013cb864a69fccbf4fbdb84113cc
Drew McMillan <drew@mcmill.co.uk>
2022-10-02 13:54:42 +0100

    intial

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Best of London</h1>
+      <h2>Croissants</h2>
+    </div>
+  );
+}
